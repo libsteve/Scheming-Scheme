a3d7559d5b5aad86521e0f9c74bb806d00f959b1
Steven Brunwasser <sbrunwasser@gmail.com>
2012-03-24 23:43:26 -0400

    working on symbol-table

--- project1/project1.scm
+++ project1/project1.scm
@@ -11,31 +11,15 @@
 ;the block depth always starts at 0
 (define block-depth 0)
 
-;return a stack-based table with the (identifier type block-depth) pair
-;(define table-push (lambda (table identifier type)
-;                     (cons '(identifier type block-depth) table)
-;                     ))
-;
-;(define table-pop (lambda (table)
-;                    (cond ((null? table) ())
-;                          (else (cdr table))
-;                          )))
-;
-;(define table-identifier-pop (lambda (table identifier)
-;                               (cond ((null? table) ())
-;                                     ((eq? (caar table) identifier) (table-pop table))
-;                                     )))
-
 ;create an empty symbol table
-(define (table-create) 'table)
+(define (table-create) ())
 
 ;check to see if the identifier is already in the table
 ;if yes, print an error message
 ;else, save the identifier and its type level
 ;return the table
 (define set-identifier (lambda (table identifier type) 
-                         (cond ((eq? 'table table) (list (set-identifier () identifier type)))
-                               ((null? table) (list (list identifier type block-depth)))
+                         (cond ((null? table) (list (list identifier type block-depth)))
                                ((not (list? (caar table)))
                                 (cond ((eq? (caar table) identifier) (display "identifier already exists") table)
                                       (else (append table (set-identifier (cdr table) identifier type)))
