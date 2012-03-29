0cdd88b47952398f87d1c485a50f5513365dd2e4
Steven Brunwasser <sbrunwasser@gmail.com>
2012-03-29 18:17:25 -0400

    yayy, the set works

--- project1/project1.scm
+++ project1/project1.scm
@@ -14,19 +14,25 @@
 ;create an empty symbol table
 (define (table-create) ())
 
+;we need da y-combinator
+;for a three parameter function
+(define set-helper (lambda (table identifier type)
+                                    (cond ((null? table) (list (list identifier type block-depth)))
+                                          ((not (eq? (caddr (car table)) block-depth)) (list (list identifier type block-depth)))
+                                          ((not (eq? (caar table) identifier)) (append (set-helper (cdr table) identifier type) table))
+                                          ((eq? (caar table) identifier) (display "identifier already exists") table)
+                                          )))
+
 ;check to see if the identifier is already in the table
 ;if yes, print an error message
 ;else, save the identifier and its type level
 ;return the table
 (define set-identifier (lambda (table identifier type) 
                          (cond ((null? table) (list (list identifier type block-depth)))
-                               ((not (list? (caar table)))
-                                (cond ((eq? (caar table) identifier) (display "identifier already exists") table)
-                                      (else (append table (set-identifier (cdr table) identifier type)))
-                                      ))
-                               (else (cons (set-identifier (car table) identifier type) (cdr table)))
-                               )
-                         ))
+                               ((not (eq? (caddr (car table)) block-depth)) (cons (list identifier type block-depth) table))
+                               (else (set-helper table identifier type))
+                               )))
+                         
 
 ;return the type and level of the specified identifier in the symbol table
 ;if the identifier is not in the table, return the empty list
