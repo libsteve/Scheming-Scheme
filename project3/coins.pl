0fc95315fa6e4aa089738059900368a342d48457
Steven Brunwasser <sbrunwasser@gmail.com>
2012-04-30 02:02:01 -0400

    started project 3, added project 4

---
+++ project3/coins.pl
@@ -0,0 +1,9 @@
+/***********************************************************
+ * File:         coins.pl
+ * Author        Steven Brunwasser, sjb9815
+ * Author		 
+ * Description:  Find the smallest amount of coins.
+ *
+ ************************************************************/
+
+ 
