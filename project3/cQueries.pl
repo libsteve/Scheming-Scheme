541c1c9fdadd5ef6ed9fdbb641bbea08121e5506
Irisshpunk <irisshpunk@irisshpunk-desktop.(none)>
2012-05-04 22:17:23 -0400

    cQueries.pl

---
+++ project3/cQueries.pl
@@ -0,0 +1,13 @@
+/***********************************************************
+ * File:         cQueries.pl
+ * Author        Steven Brunwasser, sjb9815
+ * Author	 Ethan Peters, ejp6914
+ * Author	 Peter Mikitsh, pam3961	 
+ * Description:  the queries used to test our coin code
+ *
+ ************************************************************/
+coins(387, Amount).
+coins(225, Amount).
+coins(0, Amount).
+coins(1, Amount).
+
